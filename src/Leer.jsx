b646b9f7841fde14b87e8d583476e99fa669ad3b
anahub99 <760628@unizar.es>
2021-05-13 16:16:00 +0200

    Update Leer.jsx

--- src/Leer.jsx
+++ src/Leer.jsx
@@ -24,6 +24,7 @@ const Leer = () => {
 	const [subtexto, setSubtexto] = useState("")
     useEffect(() => {
 		// e.preventDefault();
+		console.log("RENDER")
 		var url = 'https://lectorbrainbook.herokuapp.com/libro/offset/Don_Quijote_de_la_Mancha-Cervantes_Miguel.epub/0/15000'
 		// var isbn = localStorage.getItem('isbnCheck')
 		// var isbnUnquoted = isbn.replace(/['"]+/g, '');
@@ -45,34 +46,40 @@ const Leer = () => {
 			console.log(error);
 		  })
 	    }, []);
-
+		
 		const atras = () => {
 			if(inicio == '0'){
 				console.log("Ya no puedes ir mas atras")
 			}else{
-				console.log("inicio 0", inicio)
-				setInicio(inicio - 900)
-				setFinal(final - 900)
-				console.log("inicio 1", inicio)
-				// setTexto(response.data.text.substr(inicio,final))
+				console.log("inicio previo atras", inicio)
+				console.log("final previo atras", final)
+				var previoA = inicio
+				var previoB = final
+				setInicio(previoA - 900)
+				setFinal(previoB - 900)
+				console.log("inicio atras  modificado", inicio)
+				console.log("final  atras modificado", final)
+				setTexto(subtexto.text.substr(inicio,final))
+				
 			}
 		};
 
 		
+		var otro = ""
 		// Preguntar como sucede si se ha llegado al final del libro
 		const alante = () => {
 			if(final > 14100){
 				console.log("final es ", final)
 				console.log("toca volver a cargar")
 			}else{
-				console.log("inicio previo", inicio)
-				console.log("final previo", final)
+				console.log("inicio alante previo", inicio)
+				console.log("final alante previo", final)
 				var previo = inicio
 				var previo1 = final
 				setInicio(previo + 900)
 				setFinal(previo1 + 900)
-				console.log("inicio modificado", inicio)
-				console.log("final modificado", inicio)
+				console.log("inicio alante modificado", inicio)
+				console.log("final alante modificado", final)
 				setTexto(subtexto.text.substr(inicio,final))
 				
 				console.log("el texto ", texto)
@@ -113,6 +120,7 @@ const styles = StyleSheet.create({
 	},
 
 	textBook: {
+		marginLeft: 500,
 		fontSize:11,
 	}
 
