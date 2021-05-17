5cc8f7673160b2d1b8be5e4d60b858ed3640c4c6
anahub99 <760628@unizar.es>
2021-05-17 13:24:50 +0200

    Preferencias de apariencia completadas

--- src/Biblioteca.jsx
+++ src/Biblioteca.jsx
@@ -8,33 +8,120 @@ import {
 import { useState } from "react";
 import './App';
 import './styles.css';
-
+import swal from 'sweetalert';
 const Biblioteca = () => {
   const [datos, setDatos] = useState({results: []})
-
-
+  const [texto, setTexto] = useState("Hola bro")
+  const [size, setSize] = useState(14)
+  const [tone, setTone] = useState("black")
+  const [fondo, setFondo] = useState("")
+  const [tipoLetra, setTipoLetra] = useState("")
   
+  //cosas para 
+const changeTone = () =>{
+  swal( {
+    content: "input",
+    title: "Error",
+          text: "Alguno de los campos es incorrecto.",
+  })
+  .then((value) => {
+    swal(`You typed: ${value}`);
+    setTone(value)
+  });
+}
 
-
- 
-  
-
-
-  const handleLogout = () => {
-    {console.log("titulo")}
-        {console.log(datos.titulo)}
-		{datos.map((item, key) => (
-      
-      <View key={key}>
-        <Text>{item.titulo}</Text>
-        
-      </View>))
+const changeAll = () =>{
+  swal("Ajustes de visualizacion", {
+    buttons: {
+      Fondo: "Fondo",
+      Tamaño: true,
+      Tonalidad: true,
+      Tipo: true,
+    },
+  })
+  .then((value) => {
+    switch (value) {
+   
+      case "Tonalidad":
+        swal( {
+          content: "input",
+          title: "Cambiar el color de la letra",
+          text: "Ejemplo: green",
+        })
+        .then((value) => {
+          swal({
+            icon:"success",
+            title: "Los cambios se han realizado correctamente",
+          });
+          setTone(value)
+        });
+        break;
+   
+      case "Tamaño":
+        swal( {
+          content: "input",
+          title: "Cambiar el tamaño de la letra",
+          text: "Ejemplo: 11",
+        })
+        .then((value) => {
+          swal({
+            icon:"success",
+            title: "Los cambios se han realizado correctamente",
+          });
+          var anterior = parseInt(value)
+          setSize(anterior)
+        });
+        break;
+   
+      case "Fondo":
+        swal( {
+          content: "input",
+          title: "Modificar color de fondo",
+          text: "Ejemplo: black",
+        })
+        .then((value) => {
+          swal({
+            icon:"success",
+            title: "Los cambios se han realizado correctamente",
+          });
+          setFondo(value)
+        });
+        break;
+        case "Tipo":
+          swal( {
+            content: "input",
+            title: "Modificar tipo letra",
+            text: "Ejemplo: verdana",
+          })
+          .then((value) => {
+            swal({
+              icon:"success",
+              title: "Los cambios se han realizado correctamente",
+            });
+            setTipoLetra(value)
+          });
+          break;
+          default:
+          break;
     }
-	};
+  });
+}
+
   return (
     <View>
       <Navigator />
-      <h1>Biblioteca</h1>
+      <View>
+        <Text>Esta es la pagina de biblioteca</Text>
+        
+      </View>
+     
+        <Text style={{ fontSize: size, color: tone, backgroundColor: fondo, fontFamily: tipoLetra}}>
+        {texto}
+        </Text>
+        <Text onClick={changeAll}>
+        Preferencias
+        </Text>
+   
 
       </View>
   )
--- src/Leer.jsx
+++ src/Leer.jsx
@@ -1,121 +1,213 @@
 import React from 'react';
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import Navigator from './Navigator'
 import axios from 'axios'
 import {
 	StyleSheet,
 	Text,
 	View,
+	Button
 } from 'react-native';
+import swal from 'sweetalert';
 
 
 
 const Leer = () => {
-    const [texto, setTexto] = useState("");
+	const [texto, setTexto] = useState("");
 	const [inicio, setInicio] = useState(0)
 	const [final, setFinal] = useState(900)
 	const [subtexto, setSubtexto] = useState("")
 	const [limite, setLimite] = useState(14400)
-	const [tipo, setTipo] = useState(100)
-    useEffect(() => {
+	const [tipo, setTipo] = useState("textBook")
+
+	// Estados edit
+	const [size, setSize] = useState(14)
+	const [tone, setTone] = useState("black")
+	const [fondo, setFondo] = useState("")
+	const [tipoLetra, setTipoLetra] = useState("")
+
+	useEffect(() => {
 		console.log("RENDER")
 		var url = 'https://lectorbrainbook.herokuapp.com/libro/offset/Don_Quijote_de_la_Mancha-Cervantes_Miguel.epub/0/15000'
 
-		var direccion = url 
+		var direccion = url
 		const response = axios.request({
-		  url: direccion,
-		  method: 'get',	
+			url: direccion,
+			method: 'get',
 		}).then(function (response) {
 			console.log(response)
-			setTexto(response.data.text.substr(0,900))
+			setTexto(response.data.text.substr(0, 900))
 			setSubtexto(response.data)
 
 		})
-		  .catch(function (error) {
-			// handle error
-			console.log("error")
-			console.log(error);
-		  })
-	    }, []);
+			.catch(function (error) {
+				// handle error
+				console.log("error")
+				console.log(error);
+			})
+	}, []);
 
-		const obtenerTexto = () => {
-			console.log("holaaaa")
-			var comienzo = inicio
-			var acabo = inicio + 15000
-			
-			var url = 'https://lectorbrainbook.herokuapp.com/libro/offset/Don_Quijote_de_la_Mancha-Cervantes_Miguel.epub/'
+	const obtenerTexto = () => {
+		console.log("holaaaa")
+		var comienzo = inicio
+		var acabo = inicio + 15000
+
+		var url = 'https://lectorbrainbook.herokuapp.com/libro/offset/Don_Quijote_de_la_Mancha-Cervantes_Miguel.epub/'
 		var direccion = url + comienzo + "/" + acabo
-        console.log("direccion en obtener ", direccion)
-		
+		console.log("direccion en obtener ", direccion)
+
 		const response = axios.request({
-		  url: direccion,
-		  method: 'get',	
+			url: direccion,
+			method: 'get',
 		}).then(function (response) {
 			setLimite(comienzo + 15000)
 			setInicio(comienzo)
 			setTexto(response.data.text.substr(comienzo, 900))
 			setSubtexto(response.data)
-			
+
 		})
-		  .catch(function (error) {
-			// handle error
-			console.log("error")
-			console.log(error);
-		  })
-	    } ;
-		
-		
-		const atras = () => {
-			
-			if(inicio === 0){
-				console.log("Ya no puedes ir mas atras")
-			}else{
-				var start = inicio - 900
-				var end = inicio
-				setInicio(start)
-				setTexto(subtexto.text.substr(start,end))
-				
-			}
-		};
+			.catch(function (error) {
+				// handle error
+				console.log("error")
+				console.log(error);
+			})
+	};
 
-		
-		var otro = ""
-		// Preguntar como sucede si se ha llegado al final del libro
-		const alante = () => {
-			if(inicio > limite){
-				console.log("toca volver a cargar")
-				obtenerTexto()}
-			else{
+	const changeAll = () => {
+		swal("Ajustes de visualizacion", {
+			buttons: {
+				Fondo: "Fondo",
+				Tamaño: true,
+				Tonalidad: true,
+				Tipo: true,
+			},
+		})
+			.then((value) => {
+				switch (value) {
 
-				console.log("inicio alante previo", inicio)
-				// console.log("final alante previo", end)
-				var previo = inicio
-				var previo1 = inicio + 900
-				setInicio(previo + 900)				
-				console.log("primer caracter ", previo1)
-				setTexto(subtexto.text.substr(previo1,900))
-				console.log('texto', texto)
-			}
-			// return(otro)
-		};
- 
-	
+					case "Tonalidad":
+						swal({
+							content: "input",
+							title: "Cambiar el color de la letra",
+							text: "Ejemplo: green",
+						})
+							.then((value) => {
+								swal({
+									icon: "success",
+									title: "Los cambios se han realizado correctamente",
+								});
+								setTone(value)
+							});
+						break;
+
+					case "Tamaño":
+						swal({
+							content: "input",
+							title: "Cambiar el tamaño de la letra",
+							text: "Ejemplo: 11",
+						})
+							.then((value) => {
+								swal({
+									icon: "success",
+									title: "Los cambios se han realizado correctamente",
+								});
+								var anterior = parseInt(value)
+								setSize(anterior)
+							});
+						break;
+
+					case "Fondo":
+						swal({
+							content: "input",
+							title: "Modificar color de fondo",
+							text: "Ejemplo: black",
+						})
+							.then((value) => {
+								swal({
+									icon: "success",
+									title: "Los cambios se han realizado correctamente",
+								});
+								setFondo(value)
+							});
+						break;
+					case "Tipo":
+						swal({
+							content: "input",
+							title: "Modificar tipo letra",
+							text: "Ejemplo: verdana",
+						})
+							.then((value) => {
+								swal({
+									icon: "success",
+									title: "Los cambios se han realizado correctamente",
+								});
+								setTipoLetra(value)
+							});
+						break;
+					default:
+						break;
+				}
+			});
+	}
+
+	// Funciones para mover texto
+
+	const atras = () => {
+
+		if (inicio === 0) {
+			console.log("Ya no puedes ir mas atras")
+		} else {
+			var start = inicio - 900
+			var end = inicio
+			setInicio(start)
+			setTexto(subtexto.text.substr(start, end))
+
+		}
+	};
+
+
+	const alante = () => {
+		if (inicio > limite) {
+			console.log("toca volver a cargar")
+			obtenerTexto()
+		}
+		else {
+
+			console.log("inicio alante previo", inicio)
+			// console.log("final alante previo", end)
+			var previo = inicio
+			var previo1 = inicio + 900
+			setInicio(previo + 900)
+			console.log("primer caracter ", previo1)
+			setTexto(subtexto.text.substr(previo1, 900))
+			console.log('texto', texto)
+		}
+		// return(otro)
+	};
+
+
 	return (
 		<View>
 			<Navigator />
-		<h1>Pagina de leer</h1>
-		<Text onClick={alante}>
-			Boton para ir hacia alante
-		</Text>
-		<Text onClick={atras}>
-			Boton para ir hacia atras
-		</Text>
-        <View >
-            <Text style={styles.textBook}>
-                {texto}
-            </Text>
-        </View>
-		
+			<View style={{ backgroundColor: fondo, color: tone, }}>
+				<h1>Pagina de leer</h1>
+				<View id="preferencias" onClick={changeAll} style={{ fontSize: size, color: tone, fontFamily: tipoLetra,  }}>
+					Preferencias
+			</View>
+				<View id="botones" >
+					<Text onClick={alante} style={{ fontSize: size, color: tone, fontFamily: tipoLetra,  }}>
+						Boton para ir hacia alante
+			</Text>
+					<Text onClick={atras} style={{ fontSize: size, color: tone, fontFamily: tipoLetra}}  >
+						Boton para ir hacia atras
+			</Text>
+				</View>
+				<View id="texto del libro" >
+					<Text style={{ fontSize: size, color: tone, fontFamily: tipoLetra, marginLeft: 500, }} >
+						{texto}
+					</Text>
+				</View>
+			</View>
 		</View>
 
 	);
@@ -127,14 +219,14 @@ const styles = StyleSheet.create({
 		height: 200,
 	},
 	textContainer: {
-		
-		
-		textAlign:'justify',
+
+
+		textAlign: 'justify',
 	},
 
 	textBook: {
 		marginLeft: 500,
-		fontSize:11,
+		fontSize: 11,
 	}
 
 
